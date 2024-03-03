119f0ab2d178916eabe814fc99ec2c544e73ddda
Liyan Zhao <return65535@qq.com>
2024-03-03 14:59:17 +0800

    fix: return type

--- src/main/java/io/wispforest/owo/ui/event/Dismounted.java
+++ src/main/java/io/wispforest/owo/ui/event/Dismounted.java
@@ -5,15 +5,13 @@ import io.wispforest.owo.ui.core.ParentComponent;
 import io.wispforest.owo.util.EventStream;
 
 public interface Dismounted {
-    boolean onDismounted(ParentComponent parent, Component.DismountReason reason);
+    void onDismounted(ParentComponent parent, Component.DismountReason reason);
 
     static EventStream<Dismounted> newStream() {
         return new EventStream<>(subscribers -> (parent, reason) -> {
-            var anyTriggered = false;
             for (var subscriber : subscribers) {
-                anyTriggered |= subscriber.onDismounted(parent, reason);
+                subscriber.onDismounted(parent, reason);
             }
-            return anyTriggered;
         });
     }
 }
--- src/main/java/io/wispforest/owo/ui/event/Mounted.java
+++ src/main/java/io/wispforest/owo/ui/event/Mounted.java
@@ -4,15 +4,13 @@ import io.wispforest.owo.ui.core.ParentComponent;
 import io.wispforest.owo.util.EventStream;
 
 public interface Mounted {
-    boolean onMounted(ParentComponent parent, int x, int y);
+    void onMounted(ParentComponent parent, int x, int y);
 
     static EventStream<Mounted> newStream() {
         return new EventStream<>(subscribers -> (parent, x, y) -> {
-            var anyTriggered = false;
             for (var subscriber : subscribers) {
-                anyTriggered |= subscriber.onMounted(parent, x, y);
+                subscriber.onMounted(parent, x, y);
             }
-            return anyTriggered;
         });
     }
 }
